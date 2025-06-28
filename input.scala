bc5ea053d92737915e22bb12a6fdf8b9cdf7d05c
cdyashawanthraj <yashawanthrajcd@gmail.com>
2025-06-28 21:31:28 +0530

    commit feature_1 1

--- input.scala
+++ input.scala
@@ -11,6 +11,7 @@ def userInput(): Unit ={
   //println(Random.nextPrintableChar())
   println(Random.nextString(20))
   print("from dev")
+  print("changed in feature_1")
 
   println("welcome Dear user\n can i know your name please:")
   val name = StdIn.readLine()
